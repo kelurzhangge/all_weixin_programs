65f414579aedc8be5d2ebe19c035701227a70f41
kelurzhangge <1419637204@qq.com>
2018-08-25 16:39:54 +0800

    1.解决stage页面覆盖方向键和功能键的问题

--- tankbattletest/js/stage.js
+++ tankbattletest/js/stage.js
@@ -1,7 +1,7 @@
 var CONST = require("./const.js")
 import Num from './num.js'
 
-export default class Menu {
+export default class Stage {
 	constructor (context, l) {
 		this.ctx = context;
 		this.ctx.fillStyle = "#7f7f7f";
@@ -26,7 +26,12 @@ export default class Menu {
 			//temp = 15*15 灰色屏幕已经画完
 			if(this.temp == 225){
 				//78,14为STAGE字样在图片资源中的宽和高，194,208为canvas中的位置
-				this.ctx.drawImage(CONST.RESOURCE_IMAGE, CONST.POS["stageLevel"][0], CONST.POS["stageLevel"][1], 78, 14, 194, 208, 78, 14);
+				this.ctx.drawImage(CONST.RESOURCE_IMAGE, 
+					CONST.POS["stageLevel"][0], CONST.POS["stageLevel"][1], 
+					78, 14, 
+					//194, 208, 
+					(CONST.SCREEN_WIDTH*25/32 - CONST.SCREEN_WIDTH*7/32) / 2, 208,
+					78, 14);
 				//14为数字的宽和高，308, 208为canvas中的位置
 				this.levelNum.draw(this.level,308, 208);
 				//this.ctx.drawImage(RESOURCE_IMAGE,POS["num"][0]+this.level*14,POS["num"][1],14, 14,308, 208,14, 14);
@@ -39,13 +44,22 @@ export default class Menu {
 				this.dir = -1;
 				CONST.START_AUDIO.play(); 
 			}else{
-				this.ctx.fillRect(0, this.temp, 512, this.drawHeigth);
-				this.ctx.fillRect(0, 448 - this.temp - this.drawHeigth , 512, this.drawHeigth);
+				/*
+				* fillRect(x, y, width, height)的参数解释：
+				* x: 矩形路径左上角的x坐标
+				* y: 矩形路径左上角的y坐标
+				* width: 矩形路径的宽度
+				* height: 矩形路径的高度
+				* CONST.SCREEN_WIDTH*25/32  含义是：功能键B的左临界x坐标
+				* (CONST.SCREEN_WIDTH*25/32 - CONST.SCREEN_WIDTH*7/32)的含义是：功能键的左临界x坐标减去方向键右临界x坐标
+				*/
+				this.ctx.fillRect(CONST.SCREEN_WIDTH*7/32, this.temp, (CONST.SCREEN_WIDTH*25/32 - CONST.SCREEN_WIDTH*7/32), this.drawHeigth);
+				this.ctx.fillRect(CONST.SCREEN_WIDTH*7/32, 448 - this.temp - this.drawHeigth , (CONST.SCREEN_WIDTH*25/32 - CONST.SCREEN_WIDTH*7/32), this.drawHeigth);
 			}
 		}else{
 			if(this.temp >= 0){
-				this.ctx.clearRect(0, this.temp , 512, this.drawHeigth);
-				this.ctx.clearRect(0, 448 - this.temp - this.drawHeigth, 512, this.drawHeigth);
+				this.ctx.clearRect(CONST.SCREEN_WIDTH*7/32, this.temp , (CONST.SCREEN_WIDTH*25/32 - CONST.SCREEN_WIDTH*7/32), this.drawHeigth);
+				this.ctx.clearRect(CONST.SCREEN_WIDTH*7/32, 448 - this.temp - this.drawHeigth, (CONST.SCREEN_WIDTH*25/32 - CONST.SCREEN_WIDTH*7/32), this.drawHeigth);
 			}else{
 				this.isReady = true;
 			}
